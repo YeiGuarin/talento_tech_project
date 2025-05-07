96eeca44cfc5828503cad95ac1a7173696a51045
Yeimy <yjguarin@utp.edu.co>
2025-05-07 14:32:41 -0500

    Primer Commit TalentoTech Project

---
+++ FrontEnd_Project/scriptProject.js
@@ -0,0 +1,16 @@
+document.getElementById("switchBackgroundColor").addEventListener("click", ()=>{
+    document.body.classList.toggle("dark-theme");
+});
+
+// scriptProject.js
+
+
+
+document.getElementById('definition').addEventListener('click', function () {
+    this.classList.toggle('flipped');
+});
+
+
+document.getElementById('HowItWorks').addEventListener('click', function () {
+    this.classList.toggle('flipped');
+});
